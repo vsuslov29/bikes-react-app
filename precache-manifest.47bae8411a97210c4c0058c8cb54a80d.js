1ded2031b4ddf6bb40bbab5358f14ffa20fd1006
vadyanator <vadok.hohol1999@gmail.com>
2021-07-29 20:41:07 +0300

    Updates

---
+++ precache-manifest.47bae8411a97210c4c0058c8cb54a80d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1660a6b4f25e955484f7cc3d9d4f8ff0",
+    "url": "/bikes-react-app/index.html"
+  },
+  {
+    "revision": "942aaeaf5ae5c7739dba",
+    "url": "/bikes-react-app/static/css/main.0eb5fad6.chunk.css"
+  },
+  {
+    "revision": "28ff91e1d423d2853f14",
+    "url": "/bikes-react-app/static/js/2.a823ead5.chunk.js"
+  },
+  {
+    "revision": "942aaeaf5ae5c7739dba",
+    "url": "/bikes-react-app/static/js/main.4374f986.chunk.js"
+  },
+  {
+    "revision": "0bb3e5943c4bd8811781",
+    "url": "/bikes-react-app/static/js/runtime~main.f86b1404.js"
+  },
+  {
+    "revision": "5c73edf47b311b7be20d782822e9eb11",
+    "url": "/bikes-react-app/static/media/Cross.5c73edf4.svg"
+  }
+]);
